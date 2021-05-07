12be6f7e50cea75d66d7b75d0fdfa134281a78c8
khamla@siyivang <ksiyivang@gmail.com>
2021-05-07 16:25:17 +0700

    textfield successfully created:

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import Tr from './tr';
 
+import Tr from './tr';
+import TextField from '../ui/text-field';
 
 // 
 import './styles.css';
 import json from "../data/faculty.json"
+
+
+const searchableColumns = ['faculty_name'];
+
 
 
 function pageData({ data, per = 50, page = 1 }) {
@@ -21,30 +26,64 @@ export default function App() {
     sortedBy: { faculty_name: 'ascending' },
   });
 
+  // useState
+  const [query, setQuery] = useState('');
+
+  // useEffect
+
+  useEffect(() => {
+    if (!state.sortedBy) return;
+    const sortKey = Object.keys(state.sortedBy)[0];
+    const direction = state.sortedBy[sortKey];
+
+    setState((prev) => ({
+      ...prev,
+      data: prev.data.sort((a, b) => {
+        return direction === 'ascending'
+          ? a[sortKey] > b[sortKey]
+          : a[sortKey] < b[sortKey];
+      }),
+    }));
+  }, [state.sortedBy]);
+
+
+  useEffect(() => {
+    setState((prev) => ({
+      ...prev,
+      data: search(json),
+    }));
+  }, [query]);
+
+  // function
+  function search(data) {
+    return data.filter((row) =>
+      searchableColumns.some(
+        (column) =>
+          row[column]
+            .toString()
+            .toLowerCase()
+            .indexOf(query.toLowerCase()) > -1,
+      ),
+    );
+  }
+
   return (
     <div>
       Hello
 
-
+      <TextField
+        placeholder='ກະລຸນາປ້ອນຊື່ຄະນະ'
+        value={query}
+        onChange={(val) => setQuery(val)}
+      />
 
       <>
         <Tr label='ລະຫັດ' />
         <Tr
-          label='ຊື່່'
+          label='ຊື່່ຄະນະ'
           sortedBy={state.sortedBy}
-          sort={{ key: 'first_name', changer: setState }}
+          sort={{ key: 'faculty_name', changer: setState }}
         />
-        <Tr
-          label='ນາມສະກຸນ'
-          sortedBy={state.sortedBy}
-          sort={{ key: 'last_name', changer: setState }}
-        />
-        <Tr
-          label='ອີເມວ'
-          sortedBy={state.sortedBy}
-          sort={{ key: 'email', changer: setState }}
-        />
-        <Tr label='ເພດ' />
       </>
 
 
